a2ba1bd93d4e4de4c49215835ac446b41f4e3137
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-22 13:13:29 +0900

    spring security 5 이상에서 사용하는 PasswordEncoder 적용

--- src/main/java/com/example/lecspringsecurity/LecSpringSecurityApplication.java
+++ src/main/java/com/example/lecspringsecurity/LecSpringSecurityApplication.java
@@ -3,6 +3,7 @@ package com.example.lecspringsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,7 +12,7 @@ public class LecSpringSecurityApplication {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     public static void main(String[] args) {
