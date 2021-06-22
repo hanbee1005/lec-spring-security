5f98c8ea68caa0018c56cf03b771487b56637f2c
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-22 09:29:16 +0900

    init

---
+++ src/main/java/com/example/lecspringsecurity/LecSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lecspringsecurity/LecSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
