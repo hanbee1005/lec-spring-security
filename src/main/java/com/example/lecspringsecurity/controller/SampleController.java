eebf5875ad394771bab53ccbc509020918671eb9
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-22 09:45:22 +0900

    화면 추가 및 각 화면에 맞는 컨트롤러 매핑 추가

--- src/main/java/com/example/lecspringsecurity/controller/SampleController.java
+++ src/main/java/com/example/lecspringsecurity/controller/SampleController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class SampleController {
 
@@ -11,5 +13,23 @@ public class SampleController {
     public String index(Model model) {
         model.addAttribute("message", "Hello Spring Security");
         return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "Info");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "Hello " + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "Hello Admin, " + principal.getName());
+        return "admin";
     }
 }
