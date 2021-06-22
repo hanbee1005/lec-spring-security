27b15985ac63fbdeb91d63e6e71496232ea1074f
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-22 11:06:29 +0900

    Account Repository 생성

---
+++ src/main/java/com/example/lecspringsecurity/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.example.lecspringsecurity.repository;
+
+import com.example.lecspringsecurity.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+}
