82ff8cfaf88e54a432d01589a4ca2d90b3b4f019
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-22 13:39:32 +0900

    mockUser 애노테이션 처리

--- src/test/java/com/example/lecspringsecurity/controller/AccountControllerTest.java
+++ src/test/java/com/example/lecspringsecurity/controller/AccountControllerTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +25,7 @@ class AccountControllerTest {
     MockMvc mockMvc;
 
     @Test
+    @WithAnonymousUser
     public void index_anonymous() throws Exception {
         mockMvc.perform(get("/"))
                 .andDo(print())
@@ -31,23 +33,26 @@ class AccountControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "keesun", roles = "USER")
     public void index_user() throws Exception {
         // keesun 이라는 user로 인증이 되어 있다고 생각하고 진행
-        mockMvc.perform(get("/").with(user("keesun").roles("USER")))
+        mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser(username = "keesun", roles = "USER")
     public void admin_user() throws Exception {
-        mockMvc.perform(get("/admin").with(user("keesun").roles("USER")))
+        mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
 
     @Test
+    @WithMockUser(username = "keesun", roles = "ADMIN")
     public void admin_admin() throws Exception {
-        mockMvc.perform(get("/admin").with(user("keesun").roles("ADMIN")))
+        mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
