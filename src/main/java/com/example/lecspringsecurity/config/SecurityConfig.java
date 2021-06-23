9753ccd5abe01ca3508cf1d1f0febd3b9066fb1e
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-23 10:54:01 +0900

    static 자원 요청 권한 처리 무시하는 방법

--- src/main/java/com/example/lecspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/lecspringsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
@@ -20,6 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         handler.setRoleHierarchy(roleHierarchy);
 
         return handler;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/favicon.ico");
     }
 
     @Override
