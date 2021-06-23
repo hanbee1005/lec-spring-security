2fbe633101900e9af0932cca9b0916fa7e271908
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-23 10:09:08 +0900

    expressionHandler만 교체하는 방식으로 변경

--- src/main/java/com/example/lecspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/lecspringsecurity/config/SecurityConfig.java
@@ -1,35 +1,25 @@
 package com.example.lecspringsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public AccessDecisionManager accessDecisionManager() {
+    public SecurityExpressionHandler expressionHandler() {
         RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
         roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
 
         DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
         handler.setRoleHierarchy(roleHierarchy);
 
-        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
-        webExpressionVoter.setExpressionHandler(handler);
-
-        List<AccessDecisionVoter<? extends Object>> voters = Arrays.asList(webExpressionVoter);
-        return new AffirmativeBased(voters);
+        return handler;
     }
 
     @Override
@@ -39,10 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .mvcMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
-                .accessDecisionManager(accessDecisionManager())
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic();
+                .expressionHandler(expressionHandler());
+        http.formLogin();
+        http.httpBasic();
     }
 }
