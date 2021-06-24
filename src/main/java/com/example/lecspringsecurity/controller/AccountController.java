afd455d6bb3ee2118e79db430619e9bc43b6aebb
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-24 10:42:06 +0900

    form으로 회원가입하기 기능 추가

--- src/main/java/com/example/lecspringsecurity/controller/AccountController.java
+++ src/main/java/com/example/lecspringsecurity/controller/AccountController.java
@@ -2,11 +2,14 @@ package com.example.lecspringsecurity.controller;
 
 import com.example.lecspringsecurity.domain.Account;
 import com.example.lecspringsecurity.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class AccountController {
 
     private final AccountService accountService;
@@ -16,7 +19,21 @@ public class AccountController {
     }
 
     @GetMapping("/account/{role}/{username}/{password}")
+    @ResponseBody
     public Account createAccount(@ModelAttribute Account account) {
         return accountService.createNew(account);
+    }
+
+    @GetMapping("/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("account", new Account());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String processSignUp(@ModelAttribute Account account) {
+        account.setRole("USER");
+        accountService.createNew(account);
+        return "redirect:/";
     }
 }
