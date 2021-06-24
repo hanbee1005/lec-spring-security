687dd739d5a6c1f4258c645b092bcab145fb7f1b
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-24 10:52:21 +0900

    csrf 토큰 테스트 작성

--- src/test/java/com/example/lecspringsecurity/controller/AccountControllerTest.java
+++ src/test/java/com/example/lecspringsecurity/controller/AccountControllerTest.java
@@ -13,13 +13,17 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -97,6 +101,24 @@ class AccountControllerTest {
         account.setPassword(password);
         account.setRole("USER");
         return accountService.createNew(account);
+    }
+
+    @Test
+    public void signUpForm() throws Exception {
+        mockMvc.perform(get("/signup"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("_csrf")));
+    }
+
+    @Test
+    public void processSignUp() throws Exception {
+        mockMvc.perform(post("/signup")
+                .param("username", "keesun")
+                .param("password", "123")
+                .with(csrf()))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
     }
 
 }
